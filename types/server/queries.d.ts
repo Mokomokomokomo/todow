113d22198bdf8d414f970cc9ffb1f2a45679abe3
Mokomokomokomo <139135913+Mokomokomokomo@users.noreply.github.com>
2023-08-19 02:03:36 +0800

    Updated queries.d.ts
    Changed Column.column to Column.name
    Added generic typing to Column

--- types/server/queries.d.ts
+++ types/server/queries.d.ts
@@ -1,8 +1,10 @@
 import { TediousType } from "tedious"
 
-export interface Column {
-    column: string
+export interface Column<T = any> {
+    /** string representation of column (e.g. id | user.id | dbo.user.id ) */
+    name: keyof T extends T ? string : keyof T
+    /** value of the column to be set or value returned from a select */
     value: any
+    /** type of the column as described by the database */
     type: TediousType
 }
-
