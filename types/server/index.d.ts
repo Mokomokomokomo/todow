01c245c25c0578d70378b67e429b030c38938418
Mokomokomokomo <139135913+Mokomokomokomo@users.noreply.github.com>
2023-08-19 01:58:21 +0800

    Updated Server index types

--- types/server/index.d.ts
+++ types/server/index.d.ts
@@ -1,3 +1,6 @@
+import { NoteState, section } from "../../src/pages/notes/form"
+import { Datetime } from "../../src/assets/javascript/Time"
+
 export interface TokenObj {
     userid: number,
     token: string,
@@ -10,4 +13,17 @@ export interface UserObj {
     email: string,
     password: string,
     salt: string
+}
+
+export interface NoteObj{
+    user_id?: number
+    title: string
+    content: section[]
+    sched_date: Datetime
+    color?: string
+    group?: string
+}
+
+export interface SectionObj extends section {
+    note_id: number
 }
