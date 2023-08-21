5502066811d1cffea62c36b98c820405c5a91af0
Mokomokomokomo <139135913+Mokomokomokomo@users.noreply.github.com>
2023-08-22 01:08:03 +0800

    Updated Content.tsx
    - Added submit function for form

--- src/pages/notes/Content.tsx
+++ src/pages/notes/Content.tsx
@@ -1,18 +1,44 @@
 import { useState } from "react";
 import Sidebar from "../../components/sidebar";
 import CreateNote from "./form/CreateNote";
+import { NoteState } from "./form";
 
 function Content() {
     const [showCreateNote, setShowCreateNote] = useState(false);
 
-    let isEmpty = !showCreateNote
+    const createNote = async (form: NoteState) => {
+        let incompleteSections = [];
+        for(let section of form.content) {
+            if (section.description && section.time_from.hour && section.time_from.minute) {
+                continue;
+            }
+
+            incompleteSections.push(section.id);
+        }
+
+        if(incompleteSections.length > 0) {
+            return incompleteSections;
+        }
+
+        let res = await fetch('/api/note/create', {
+            body: JSON.stringify(form),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "post"
+        }).then(dat => dat.json());
+
+        if (res.success) {
+            setShowCreateNote(false);
+        }
+    }
 
     return (
         <div className="content">
             <Sidebar createNote={() => {setShowCreateNote(true)}} />
-            <div className={`main_content${isEmpty ? ' no_notes': ''}`}>
-                {showCreateNote && <CreateNote />}
-                {isEmpty && <span>No Notes Found. Create A new one</span>}
+            <div className={`main_content${!showCreateNote ? ' no_notes': ''}`}>
+                {showCreateNote && <CreateNote submitForm={createNote} />}
+                {!showCreateNote && <span>No Notes Found. Create A new one</span>}
             </div>
         </div>
     )
