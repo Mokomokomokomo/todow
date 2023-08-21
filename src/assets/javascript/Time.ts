f577467bfb07dc9963453b310e88a2698503a0d4
Mokomokomokomo <139135913+Mokomokomokomo@users.noreply.github.com>
2023-08-22 01:07:03 +0800

    Updated Time.ts
    - Added Time interface

--- src/assets/javascript/Time.ts
+++ src/assets/javascript/Time.ts
@@ -1,29 +1,26 @@
-import {
-    Month, 
-    monthFormat, 
-    Datetime, 
-    BinaryTC,
-    Day,
-    dtFormat,
-} from '../../../types/client/Time'; 
-
-const sm: Month[] = [
+const sm = [
     "Jan", "Feb", "March",
     "April", "May", "Jun",
     "Jul", "Aug", "Sep",
     "Oct", "Nov", "Dec"
-];
+] as const;
 
-const fm: Month[] = [
+const fm = [
     "January", "February", "March",
     "April", "May", "June",
     "July", "August", "September",
     "October", "November", "December"
-]
+] as const;
 
-export const days: Day[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+export type Month = typeof sm[number] | typeof fm[number];
 
-export const getMonths = (monthFormat: monthFormat = "short"): Month[] => {
+export const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] as const;
+
+export type Day = typeof days[number];
+
+type MonthFormat = "short" | "full";
+
+export const getMonths = (monthFormat: MonthFormat = "short"): readonly Month[] => {
     return (monthFormat == 'short') ? sm : fm
 }
 
@@ -32,7 +29,11 @@ export const getMonths = (monthFormat: monthFormat = "short"): Month[] => {
  * @param month String representation of the month, can be short or full form 
  * @param year If provided, will check for leap year and adjust accordingly
  */
-export const getDaysInMonth = (month: Month, year?: number) => {
+export const getDaysInMonth = (month: Month, year?: number | string) => {
+    if (typeof year == 'string') {
+        year = parseInt(year);
+    }
+
     const md: number[] = [
         31, (year && year % 4 == 0) ? 29 : 28, 31,
         30, 31, 30,
@@ -55,12 +56,45 @@ export const padZeroes = (digit: number, maxLen: number) => {
     return digit.toString().padStart(maxLen, "0");
 }
 
-export const getDateTime = (format: Partial<dtFormat> = {}): Datetime => {
-    let initFormat: dtFormat = {
+type alphanumeric = "number" | "string";
+export type BinaryTC = "am" | "pm";
+export type TimeCycle = "binary" | "military";
+
+export interface DTFormat {
+    year: alphanumeric
+    month: MonthFormat
+    timeCycle: TimeCycle
+    day_of_month: alphanumeric
+    hour: alphanumeric
+    minute: alphanumeric
+    second: alphanumeric
+}
+
+export interface Datetime {
+    year: number | string,
+    month: Month,
+    /** Current Day of the month */
+    day_of_month: number | string,
+    hour: number | string,
+    time_of_day?: BinaryTC,
+    minute: number | string,
+    second: number | string,
+    /** Current Day of the week in string representation (Monday etc.) */
+    day: Day,
+}
+
+export interface Time {
+    hour: number | string
+    minute: number | string
+    tc: BinaryTC
+}
+
+export const getDateTime = (hourOffset: number = 0, format?: DTFormat): Datetime => {
+    let initFormat: DTFormat = {
         year: "number",
         month: "short",
-        day: "string",
         timeCycle: "binary",
+        day_of_month: "string",
         hour: "string",
         minute: "string",
         second: "string",
@@ -69,6 +103,9 @@ export const getDateTime = (format: Partial<dtFormat> = {}): Datetime => {
     format = {...initFormat, ...format};
 
     let currDate = new Date();
+    if(hourOffset > 0) {
+        currDate.setHours(currDate.getHours() + hourOffset);
+    }
 
     let month = currDate.getMonth();
     let date = currDate.getDate();
@@ -85,7 +122,7 @@ export const getDateTime = (format: Partial<dtFormat> = {}): Datetime => {
     return {
         year: currDate.getFullYear(),
         month: (format.month == "short") ? sm[month] : fm[month],
-        day_of_month: (format.day == "string") ? padZeroes(date, 2) : date,
+        day_of_month: (format.day_of_month == "string") ? padZeroes(date, 2) : date,
         hour: (format.hour == "string") ? padZeroes(hour, 2) : hour,
         time_of_day: (format.timeCycle == "binary") ? timeCycle : undefined,
         minute: (format.minute == "string") ? padZeroes(minute, 2) : minute,
